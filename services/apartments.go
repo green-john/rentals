e30ef1079ff3668d66b4ef6028064a4f37a247d2
Andres Cardozo <andresfeliperu@gmail.com>
2019-02-03 22:04:35 +0100

    Refactor things into services
    Do some pretty big refactoring to extract things into
    services and no longer use resources.

---
+++ services/apartments.go
@@ -0,0 +1,221 @@
+package services
+
+import (
+	"errors"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"net/url"
+	"reflect"
+	"rentals"
+	"strconv"
+)
+
+var NotFoundError = errors.New("entity not found")
+
+type ApartmentCreateInput struct {
+	rentals.Apartment
+}
+
+type ApartmentCreateOutput struct {
+	rentals.Apartment
+}
+
+type ApartmentReadInput struct {
+	// ID to lookup the apartment
+	Id string
+}
+
+type ApartmentReadOutput struct {
+	rentals.Apartment
+}
+
+type ApartmentFindInput struct {
+	Query string
+}
+
+type ApartmentFindOutput struct {
+	Apartments []rentals.Apartment
+}
+
+func (o *ApartmentFindOutput) Public() interface{} {
+	return o.Apartments
+}
+
+type ApartmentUpdateInput struct {
+	Id   string
+	Data map[string]interface{}
+}
+
+type ApartmentUpdateOutput struct {
+	rentals.Apartment
+}
+
+type ApartmentDeleteInput struct {
+	Id string
+}
+
+type ApartmentDeleteOutput struct {
+	Message string
+}
+
+var JsonTagsToFilter = map[string]string{
+	"floor_area_meters":   getJsonTag(rentals.Apartment{}, "FloorAreaMeters"),
+	"price_per_month_usd": getJsonTag(rentals.Apartment{}, "PricePerMonthUsd"),
+	"room_count":          getJsonTag(rentals.Apartment{}, "RoomCount"),
+}
+
+//var ApartmentJsonTags = []string{
+//	getJsonTag(rentals.Apartment{}, "Name"),
+//	getJsonTag(rentals.Apartment{}, "Desc"),
+//	getJsonTag(rentals.Apartment{}, "RealtorId"),
+//	getJsonTag(rentals.Apartment{}, "FloorAreaMeters"),
+//	getJsonTag(rentals.Apartment{}, "PricePerMonthUsd"),
+//	getJsonTag(rentals.Apartment{}, "RoomCount"),
+//	getJsonTag(rentals.Apartment{}, "Latitude"),
+//	getJsonTag(rentals.Apartment{}, "Longitude"),
+//	getJsonTag(rentals.Apartment{}, "Available"),
+//}
+
+type ApartmentService interface {
+	Create(ApartmentCreateInput) (*ApartmentCreateOutput, error)
+	Read(ApartmentReadInput) (*ApartmentReadOutput, error)
+	Find(ApartmentFindInput) (*ApartmentFindOutput, error)
+	Update(ApartmentUpdateInput) (*ApartmentUpdateOutput, error)
+	Delete(ApartmentDeleteInput) (*ApartmentDeleteOutput, error)
+}
+
+type dbApartmentService struct {
+	Db *gorm.DB
+}
+
+func (ar *dbApartmentService) Create(in ApartmentCreateInput) (*ApartmentCreateOutput, error) {
+	ar.Db.Create(&(in.Apartment))
+
+	return &ApartmentCreateOutput{Apartment: in.Apartment}, nil
+}
+
+func (ar *dbApartmentService) Read(in ApartmentReadInput) (*ApartmentReadOutput, error) {
+	apartment, err := getApartment(in.Id, ar.Db)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ApartmentReadOutput{Apartment: *apartment}, nil
+}
+
+func (ar *dbApartmentService) Find(input ApartmentFindInput) (*ApartmentFindOutput, error) {
+	values, err := url.ParseQuery(input.Query)
+	if err != nil {
+		return nil, err
+	}
+
+	tx := ar.Db.New()
+	for dbField, jsonTag := range JsonTagsToFilter {
+		if v, ok := values[jsonTag]; ok {
+			if !ok || len(v) == 0 {
+				continue
+			}
+
+			// TODO potential for injection here
+			tx = tx.Where(fmt.Sprintf("%s = ?", dbField), v[0])
+		}
+	}
+
+	var apartments []rentals.Apartment
+	tx.Find(&apartments)
+	return &ApartmentFindOutput{Apartments: apartments}, nil
+}
+
+func (ar *dbApartmentService) Update(input ApartmentUpdateInput) (*ApartmentUpdateOutput, error) {
+	apartment, err := getApartment(input.Id, ar.Db)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := updateFields(apartment, input.Data); err != nil {
+		return nil, err
+	}
+
+	// Save to DB
+	if err = ar.Db.Save(&apartment).Error; err != nil {
+		return nil, err
+	}
+	return &ApartmentUpdateOutput{Apartment: *apartment}, nil
+}
+
+func (ar *dbApartmentService) Delete(input ApartmentDeleteInput) (*ApartmentDeleteOutput, error) {
+	apartment, err := getApartment(input.Id, ar.Db)
+	if err != nil {
+		return nil, err
+	}
+
+	ar.Db.Delete(&apartment)
+	return &ApartmentDeleteOutput{Message: "success"}, nil
+}
+
+func getApartment(id string, db *gorm.DB) (*rentals.Apartment, error) {
+	intId, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, err
+	}
+
+	var apartment rentals.Apartment
+	if err = db.First(&apartment, intId).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, NotFoundError
+		}
+		return nil, err
+	}
+
+	return &apartment, nil
+}
+
+func updateFields(apartment *rentals.Apartment, data map[string]interface{}) error {
+	if v, ok := data["name"]; ok {
+		apartment.Name = v.(string)
+	}
+
+	if v, ok := data["description"]; ok {
+		apartment.Desc = v.(string)
+	}
+
+	if v, ok := data["floorAreaMeters"]; ok {
+		apartment.FloorAreaMeters = v.(float32)
+	}
+
+	if v, ok := data["pricePerMonthUSD"]; ok {
+		apartment.PricePerMonthUsd = v.(float32)
+	}
+
+	if v, ok := data["roomCount"]; ok {
+		apartment.RoomCount = v.(int)
+	}
+
+	if v, ok := data["latitude"]; ok {
+		apartment.Latitude = v.(float32)
+	}
+
+	if v, ok := data["longitude"]; ok {
+		apartment.Longitude = v.(float32)
+	}
+
+	if v, ok := data["available"]; ok {
+		apartment.Available = v.(bool)
+	}
+
+	return nil
+}
+
+func getJsonTag(v interface{}, fieldName string) string {
+	t := reflect.TypeOf(v)
+	field, ok := t.FieldByName(fieldName)
+	if !ok {
+		return ""
+	}
+
+	return field.Tag.Get("json")
+}
+
+func NewDbApartmentService(db *gorm.DB) *dbApartmentService {
+	return &dbApartmentService{Db: db}
+}
--- services/apartments_test.go
+++ services/apartments_test.go
@@ -1,7 +1,6 @@
-package transport
+package services
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/jinzhu/gorm"
 	"rentals"
@@ -17,10 +16,10 @@ func TestFindApartment(t *testing.T) {
 	db.AutoMigrate(rentals.DbModels...)
 	defer db.DropTableIfExists(rentals.DbModels...)
 
-	aptResource := &ApartmentResource{Db: db}
+	aptResource := &dbApartmentService{Db: db}
 
-	tst.Ok(t, createRealtor(db))
-	tst.Ok(t, createApartments(aptResource))
+	createRealtor(t, db)
+	createApartments(t, aptResource)
 
 	for _, elt := range []struct {
 		query     string
@@ -35,14 +34,13 @@ func TestFindApartment(t *testing.T) {
 	} {
 		t.Run(fmt.Sprintf("%s -> %v", elt.query, elt.resultIds), func(t *testing.T) {
 			// Act
-			var retApts []rentals.Apartment
-			res, err := aptResource.Find(elt.query)
+			res, err := aptResource.Find(ApartmentFindInput{Query: elt.query})
 			tst.Ok(t, err)
 
-			err = json.Unmarshal(res, &retApts)
-			tst.Ok(t, err)
+			//err = json.Unmarshal(res, &retApts)
+			//tst.Ok(t, err)
 
-			for idx, apt := range retApts {
+			for idx, apt := range res.Apartments {
 				tst.Assert(t, apt.Name == elt.resultIds[idx],
 					fmt.Sprintf("Expected %s, got %s", elt.resultIds[idx], apt.Name))
 			}
@@ -60,47 +58,42 @@ func TestFindApartment(t *testing.T) {
 //    2      1      2
 //    2      2      1
 //    2      2      2
-func createApartments(r *ApartmentResource) error {
+func createApartments(t *testing.T, s *dbApartmentService) {
 	for area := 1; area <= 2; area++ {
 		for price := 1; price <= 2; price++ {
 			for rooms := 1; rooms <= 2; rooms++ {
 				name := fmt.Sprintf("%d|%d|%d", area, price, rooms)
 				payload := newApartmentPayload(name, name, float32(area), float32(price), rooms, 1)
-				_, err := r.Create(payload)
-
-				if err != nil {
-					return err
-				}
+				_, err := s.Create(payload)
+				tst.Ok(t, err)
 			}
 		}
 	}
-
-	return nil
 }
 
-func newApartmentPayload(name, desc string, area, price float32, roomCount int, realtorId uint) []byte {
-	return []byte(fmt.Sprintf(
-		`{
-"name":"%s",
-"description": "%s",
-"floorAreaMeters": %f,
-"pricePerMonthUSD": %f,
-"roomCount": %d,
-"latitude": 41.761536,
-"longitude": 12.315237,
-"available": true,
-"realtorId": %d}`, name, desc, area, price, roomCount, realtorId))
-}
-
-func createRealtor(db *gorm.DB) error {
-	usrResource := &UserResource{Db: db}
-
-	usrData := []byte(`{"username":"user", "password": "pass", "role": "realtor"}`)
-	_, err := usrResource.Create(usrData)
-
-	if err != nil {
-		return err
+func newApartmentPayload(name, desc string, area, price float32, roomCount int, realtorId uint) ApartmentCreateInput {
+	return ApartmentCreateInput{
+		Apartment: rentals.Apartment{
+			Name:             name,
+			Desc:             desc,
+			FloorAreaMeters:  area,
+			PricePerMonthUsd: price,
+			RoomCount:        roomCount,
+			RealtorId:        realtorId,
+			Longitude:        34.3222223,
+			Latitude:         21.233449,
+			Available:        true,
+		},
 	}
+}
 
-	return nil
+func createRealtor(t *testing.T, db *gorm.DB) {
+	usrService := NewDbUserService(db)
+
+	_, err := usrService.Create(UserCreateInput{
+		Username: "user",
+		Password: "pass",
+		Role:     "realtor",
+	})
+	tst.Ok(t, err)
 }
